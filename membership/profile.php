31eb02d2c088335048b44cd50993a824007dfd7c
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-12 05:31:11 +0200

    fix(profile.php): remove redundant font family from stylesheet link

--- membership/profile.php
+++ membership/profile.php
@@ -32,7 +32,7 @@ require_once '../scripts/connection.php';
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
   <script src='../select2/dist/js/select2.min.js' type='text/javascript'></script>
   <!-- Vendor CSS Files -->
   <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
